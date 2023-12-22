a7b577c84363e61aede27d85a05311e621be438d
TranVanPhuQuy-20130378 <tranvanphuquy01266984818@gmail.com>
2023-12-22 12:07:08 +0700

    thêm phương thức querry ,thêm các lớp

--- src/main/java/service/AdminService_MT.java
+++ src/main/java/service/AdminService_MT.java
@@ -43,4 +43,59 @@ public class AdminService_MT {
         }
         return null;
     }
+    public static boolean updatePassword(String username, String new_pass) {
+
+        DbConnection connectDB = DbConnection.getInstance();
+        String sql = "update account_admin set passwordAD = ?, time_change_pass = current_timestamp() where username = ?";
+        PreparedStatement preState = connectDB.getPreparedStatement(sql);
+        try {
+            preState.setString(1, new_pass);
+            preState.setString(2, username);
+            int update = preState.executeUpdate();
+            if (update > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connectDB.close();
+        }
+
+        return false;
+    }
+
+    public static boolean updateAccount(Admin admin) {
+        DbConnection connectDB = DbConnection.getInstance();
+        AdminDAO dao = new AdminDAO();
+        try {
+            return dao.updateAccount(connectDB, admin);
+        } finally {
+            connectDB.close();
+        }
+    }
+    public static List<Admin> getAllAdmin() {
+        DbConnection connectDB = DbConnection.getInstance();
+        AdminDAO dao = new AdminDAO();
+        try {
+            return dao.getAllAdmin(connectDB);
+        } finally {
+            connectDB.close();
+        }
+    }
+
+    public static List<Object> getAllRoleAdminAndStatusAcc() {
+        List<Object> result = new ArrayList<>();
+        DbConnection connectDB = DbConnection.getInstance();
+        AdminDAO dao = new AdminDAO();
+        try {
+            List<RoleAdmin> roleAdmins = dao.getAllRoleAdmin(connectDB);
+            List<StatusAcc> statusAccs = dao.getAllStatusAcc(connectDB);
+            result.add(roleAdmins);
+            result.add(statusAccs);
+        } finally {
+            connectDB.close();
+        }
+        return result;
+    }
+
 }
